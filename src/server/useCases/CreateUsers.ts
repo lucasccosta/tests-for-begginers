f9bfac44ea0165c9131c2683d09d5876f2a25b5b
lucas_ccg@hotmail.com <lucas_ccg@hotmail.com>
2022-11-23 17:13:15 -0300

    fixing the files name

--- src/server/useCases/CreateUsers.ts
+++ src/server/useCases/CreateUsers.ts
@@ -5,11 +5,10 @@ class CreateUsers {
   async execute(name: string, email: string, password: string) {
     const usersRepository = new UsersRepository();
     const userAlreadyExists = await usersRepository.getByEmail(email);
-    // if (userAlreadyExists)
-    //   throw new Error("User already exists! Create another one.");
+    if (userAlreadyExists)
+      throw new Error("User already exists! Create another one.");
 
     const user = new User(name, email, password);
-
     await usersRepository.create(user);
   }
 }
