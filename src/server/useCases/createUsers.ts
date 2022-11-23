718b76051693b0648ef1d54a4d8d7bff81425d9a
lucas_ccg@hotmail.com <lucas_ccg@hotmail.com>
2022-11-22 21:21:29 -0300

    creating use case file

---
+++ src/server/useCases/createUsers.ts
@@ -0,0 +1,17 @@
+import { User } from "../domain/user/User";
+import { UsersRepository } from "../repository/user/UsersRepository";
+
+class CreateUsers {
+  async execute(name: string, email: string, password: string) {
+    const usersRepository = new UsersRepository();
+    const userAlreadyExists = await usersRepository.getByEmail(email);
+    // if (userAlreadyExists)
+    //   throw new Error("User already exists! Create another one.");
+
+    const user = new User(name, email, password);
+
+    await usersRepository.create(user);
+  }
+}
+
+export { CreateUsers };
