5b341117fe5309aae319197f53fcb05940d26d04
lucas_ccg@hotmail.com <lucas_ccg@hotmail.com>
2022-11-23 17:16:27 -0300

    removing constructor and changing primary key from id to email

--- src/server/database/sequelize/model/UserModel.ts
+++ src/server/database/sequelize/model/UserModel.ts
@@ -1,27 +1,22 @@
-import { Table, Column, PrimaryKey, Model } from "sequelize-typescript";
-import { v4 as uuid } from "uuid";
+import {
+  Table,
+  Column,
+  PrimaryKey,
+  Model,
+  Default,
+} from "sequelize-typescript";
 
 @Table({
   tableName: "users",
   timestamps: false,
 })
 class UsersModel extends Model {
-  constructor() {
-    super();
-    if (!this.id) {
-      this.id = uuid();
-    }
-  }
-
   @PrimaryKey
-  @Column
-  declare id: string;
+  @Column({ allowNull: false })
+  declare email: string;
 
   @Column({ allowNull: false })
   declare name: string;
-
-  @Column({ allowNull: false })
-  declare email: string;
 
   @Column({ allowNull: false })
   declare password: string;
