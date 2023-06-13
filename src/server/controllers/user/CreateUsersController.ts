28f7d25b3f69b36c2629e931d8d90f0b0736070d
Lucas <lucas_ccg@hotmail.com>
2023-06-13 13:47:14 -0300

    changing to proper status code

--- src/server/controllers/user/CreateUsersController.ts
+++ src/server/controllers/user/CreateUsersController.ts
@@ -8,7 +8,7 @@ class CreateUsersController {
     try {
       const useCase = new CreateUsers();
       const createUsers = await useCase.execute(name, email, password);
-      return response.status(200).send("User created successfully!");
+      return response.status(201).send("User created successfully!");
     } catch (error) {
       // @ts-ignore
       return response.status(500).send(error.message);
