081ccaf4c18a8f97acb32ade9f2eebfaf450b875
Lucas <lucas_ccg@hotmail.com>
2023-06-20 16:31:32 -0300

    changing return

--- src/server/useCases/CreateUsers.ts
+++ src/server/useCases/CreateUsers.ts
@@ -1,13 +1,14 @@
 import { UsersRepository } from "../repository/user/UsersRepository";
 
 class CreateUsers {
-  async execute(name: string, email: string, password: string) {
+  async execute(name: string, email: string, password: string): Promise<string> {
     const usersRepository = new UsersRepository();
     const userAlreadyExists = await usersRepository.getByEmail(email);
-    if (userAlreadyExists)
-      throw new Error("User already exists! Create another one.");
+    if (userAlreadyExists) throw new Error("User already exists! Create another one.");
 
     await usersRepository.create(name, email, password);
+
+    return "User created successfully!"
   }
 }
 
