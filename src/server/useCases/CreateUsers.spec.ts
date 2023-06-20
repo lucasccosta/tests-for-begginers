b98c18f7d834f6f40e61317e7795d875b2c270f7
Lucas <lucas_ccg@hotmail.com>
2023-06-20 16:31:13 -0300

    add global invocations for instances and add new test case

--- src/server/useCases/CreateUsers.spec.ts
+++ src/server/useCases/CreateUsers.spec.ts
@@ -19,18 +19,39 @@ describe("use case unit tests", () => {
   afterEach(async () => {
     await sequelize.close();
   });
+
+  const useCase = new CreateUsers();
+  const usersRepository = new UsersRepository();
+
   it("should create an user", async () => {
     const useCase = new CreateUsers();
     const userDomain = new User("Usuario Teste", "user@teste.com", "12345678");
-    await useCase.execute(
+    const useCaseResponse = await useCase.execute(
       userDomain.name,
       userDomain.email,
       userDomain.password
     );
 
-    const usersRepository = new UsersRepository();
     const userCreated = await usersRepository.getByEmail("user@teste.com");
 
     expect(userCreated).toStrictEqual(userDomain);
+    expect(useCaseResponse).toBe("User created successfully!")
+  });
+
+  it("should return user already exists error", () => {
+    expect(async () => {
+
+      const user1 = new User("Usuario Teste 1", "user@teste.com", "12345678");
+      await usersRepository.create(user1.name, user1.email, user1.password)
+  
+      const user2 = new User("Usuario Teste 2", "user@teste.com", "002528528");
+      await useCase.execute(
+        user2.name,
+        user2.email,
+        user2.password
+      );
+      
+      await usersRepository.getByEmail("user@teste.com");
+    }).rejects.toThrow("User already exists! Create another one.");
   });
 });
